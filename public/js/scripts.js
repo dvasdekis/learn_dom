7dc296cfd2bd7689695eb0a4419565f47e712fd0
dvasdekis <dimitri@vasdekis.com.au>
2019-08-01 12:55:50 +1000

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,7 @@
-let button = document.getElementById('changeBackground');
+/* Assign the HTML button element with id of 'changeBackground' to a JS variable */
+let mybutton = document.getElementById('changeBackground')
+
+/* Tell the button to listen for a click, and do something when clicked */
+mybutton.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'fuchsia'
+})
